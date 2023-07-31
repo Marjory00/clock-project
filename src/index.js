b7e0f4350f26232d40175b153c7e4e22ee64008c
Marjory Marquez <samaterra@hotmail.com>
2023-07-31 18:55:29 -0400

    setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
+
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
+
 import App from './App';
+import reducer from './reducer';
+import registerServiceWorker from './registerServiceWorker';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
+registerServiceWorker();
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
